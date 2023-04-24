39160990337fbd1f1728f0ef0ccc8b6b3626b3cf
Laptop <98688203+LaptopCat@users.noreply.github.com>
2023-04-24 17:58:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,32 +13,60 @@ from threading import Thread, Event
 from helpers import console
 from sys import version as pyver
 
-__version__ = "1.2"
+print = console.print
+__version__ = "2.0"
 plugin_regex = compile(r"\{%plugin:(.*?)%\}")
 telegram = Config.telegram
 script = Config.script
-print = console.print
-old = ""
-print(Align.center("""
-[bold blue]
+script.logstrings = getattr(script, "logstrings", None)
+telegram.auth.mode = getattr(telegram.auth, "mode", "S")
+script.splitter = getattr(script, "splitter", "|")
+script.delay = getattr(script, "delay", 20)
+script.max_length = getattr(script, "max_length", 70)
+default_logstrings = {
+      "banner": """[bold blue]
   _      _______      ________ ____ _____ ____  
  | |    |_   _\ \    / /  ____|  _ \_   _/ __ \ 
  | |      | |  \ \  / /| |__  | |_) || || |  | |
  | |      | |   \ \/ / |  __| |  _ < | || |  | |
  | |____ _| |_   \  /  | |____| |_) || || |__| |
  |______|_____|   \/   |______|____/_____\____/ 
-[/bold blue]
-"""))
-print(Align.center("[bold blue]Livebio[/bold blue] Version [bold blue]{}[/bold blue] | [blue]Pyt[/blue][bold bright_yellow]hon[/bold bright_yellow] Version [bold blue]{}[/bold blue]".format(__version__, pyver.split(" (")[0])))
-print(Align.center("\n[bold blue][link=https://github.com/LaptopCat/livebio-tg]GitHub[/link][/bold blue]\n"))
+[/bold blue]""",
+      "versions": "[bold blue]Livebio[/bold blue] Version [bold blue]{}[/bold blue] | [blue]Pyt[/blue][bold bright_yellow]hon[/bold bright_yellow] Version [bold blue]{}[/bold blue]",
+      "github": "\n[bold blue][link=https://github.com/LaptopCat/livebio-tg]GitHub[/link][/bold blue]\n",
+      "loading": "Loading plugins...",
+      "nometa": "Plugin Metadata is invalid",
+      "nogather": "Plugin does not have an async gather function",
+      "nopostprocess": "Plugin does not have an async postprocess function",
+      "nosetup": "Plugin does not have a setup function",
+      "loadedplugin": "[MAIN] Loaded plugin [link={}][bold blue]{}[/bold blue][/link] v[bold blue]{}[/bold blue] (by [bold blue]{}[/bold blue])",
+      "loadfailed": "[bold red][MAIN] Failed to load [bold blue]{}[/bold blue] plugin! ([bold blue]{}[/bold blue]: [bold blue]{}[/bold blue])[/bold red]",
+      "loaded": "[MAIN] Loaded [bold blue]{}[/bold blue] plugin(s) in [bold blue]{}[/bold blue]s!\n",
+      "generating": "Generating status...",
+      "notfound": "Plugin {} was not found!",
+      "didntgenerate": "[bold red]\[{}] Failed to generate content! ([bold blue]{}[/bold blue]: [bold blue]{}[/bold blue])[/bold red]",
+      "didntpostprocess": "[bold red][{}] Failed to postprocess! ([bold blue]{}[/bold blue]: [bold blue]{}[/bold blue])[/bold red]",
+      "changing": "[MAIN] Changing bio to [bold blue]{}[/bold blue]",
+      "didntedit": "[bold red]\[MAIN] Failed to edit bio to [bold blue]{}[/bold blue]! ([bold blue]{}[/bold blue]: [bold blue]{}[/bold blue])[/bold red]",
+      "edited": "[MAIN] Changed bio to [bold blue]{}[/bold blue]",
+      "cached": "[MAIN] Generated bio is identical to cached bio ([bold blue]{}[/bold blue]), not changing bio",
+      "sleeping": "Sleeping...",
+      "didntauth": "[MAIN] Failed to authenticate."
+}
+def logstring(name):
+  return getattr(script.logstrings, name, default_logstrings[name])
+old = ""
+print(Align.center(logstring("banner")))
+print(Align.center(logstring("versions").format(__version__, pyver.split(" (")[0])))
+print(Align.center(logstring("github")))
 async def check_auth():
   try:
     if telegram.auth.mode == "F":
       async with TelegramClient("livebio", telegram.app.id, telegram.app.hash, receive_updates=False):
-        ...
+        pass
     elif telegram.auth.mode == "S":
       async with TelegramClient(StringSession(telegram.auth.string), telegram.app.id, telegram.app.hash, receive_updates=False):
-        ...
+        pass
     else:
       return False, None
   except BaseException as e:
@@ -50,7 +78,7 @@ stopper_event = Event()
 def add_plugins():
   start = perf_counter()
   global plugins
-  with console.status("Loading plugins...", spinner="point"):
+  with console.status(logstring("loading"), spinner="point"):
     for plugin in listdir("./plugins"):
       if plugin.endswith(".plugin.py"):
         try:
@@ -59,27 +87,27 @@ def add_plugins():
           spec.loader.exec_module(plugins[plugin])
           loaded = plugins[plugin]
           try:
-            assert type(loaded.__plugin__) is dict, "Plugin Metadata is invalid"
-            assert iscoroutinefunction(loaded.gather), "Plugin does not have an async gather function"
-            assert iscoroutinefunction(loaded.postprocess), "Plugin does not have an async postprocess function"
-            assert type(loaded.setup).__name__ == "function", "Plugin does not have a setup function"
+            assert type(loaded.__plugin__) is dict, logstring("nometa")
+            assert iscoroutinefunction(loaded.gather), logstring("nogather")
+            assert iscoroutinefunction(loaded.postprocess), logstring("nopostprocess")
+            assert type(loaded.setup).__name__ == "function", logstring("nosetup")
             manifest = loaded.__plugin__
           except Exception as e:
             del plugins[plugin]
             raise e
           Thread(target=loaded.setup, daemon=True, args=(stopper_event, )).start()
-          console.log("[MAIN] Loaded plugin [link={}][bold blue]{}[/bold blue][/link] v[bold blue]{}[/bold blue] (by [bold blue]{}[/bold blue])".format(manifest.get("link", "https://github.com/LaptopCat/livebio-tg"), manifest.get("name"), manifest.get("version"), manifest.get("author")))
+          console.log(logstring("loadedplugin").format(manifest.get("link", "https://github.com/LaptopCat/livebio-tg"), manifest.get("name"), manifest.get("version"), manifest.get("author")))
         except Exception as e:
           try: del plugins[plugin]
           except: pass
-          console.log("[bold red][MAIN] Failed to load [bold blue]{}[/bold blue] plugin! ([bold blue]{}[/bold blue]: [bold blue]{}[/bold blue])[/bold red]".format(plugin.strip(".plugin.py"), type(e).__name__, str(e)))
-  console.log("[MAIN] Loaded [bold blue]{}[/bold blue] plugin(s) in [bold blue]{}[/bold blue]s!\n".format(len(plugins), round(perf_counter()-start, ndigits=4)))
+          console.log(logstring("loadfailed").format(plugin.strip(".plugin.py"), type(e).__name__, str(e)))
+  console.log(logstring("loaded").format(len(plugins), round(perf_counter()-start, ndigits=4)))
 
 
 async def main():
   global old
   while True:
-    with console.status("Generating status...", spinner="point"):
+    with console.status(logstring("generating"), spinner="point"):
       generated = str(script.template)
       used_plugins = findall(plugin_regex, script.template)
       contributed_plugins = []
@@ -91,7 +119,7 @@ async def main():
           try:
             this_plugin = plugins[plugin + ".plugin.py"]
           except KeyError:
-            raise KeyError("Plugin {} was not found!".format(plugin))
+            raise KeyError(logstring("notfound").format(plugin))
           add = await this_plugin.gather()
           if add != None and add.strip() != "":
             generated=generated.replace(slug, add+((" {}{}".format(script.splitter, " " if used_plugins.index(plugin) != len(used_plugins)-1 else "")) if unsplit is False else ""), 1)
@@ -99,9 +127,9 @@ async def main():
           else:
             generated=generated.replace(" "+slug, "", 1).replace(slug, "", 1)
         except Exception as e:
-          console.log("[bold red]\[{}] Failed to generate content! ([bold blue]{}[/bold blue]: [bold blue]{}[/bold blue])[/bold red]".format(plugin, type(e).__name__, str(e)))
+          raise e
+          console.log(logstring("didntgenerate").format(plugin, type(e).__name__, str(e)))
           generated=generated.replace(" "+slug, "", 1).replace(slug, "", 1)
-          pass
       for plugin in contributed_plugins:
          try:
           unsplit = "UNSPLIT" in plugin[0]
@@ -115,9 +143,8 @@ async def main():
           else:
             pass
          except Exception as e:
-           name = plugin[0]
-           console.log("[bold red][{}] Failed to postprocess! ([bold blue]{}[/bold blue]: [bold blue]{}[/bold blue])[/bold red]".format(name, type(e).__name__, str(e)))
-    with Live("[MAIN] Changing bio to [bold blue]{}[/bold blue]".format(generated), console=console, refresh_per_second=1) as live:
+           console.log(logstring("didntpostprocess").format(plugin[0], type(e).__name__, str(e)))
+    with Live(logstring("changing").format(generated), console=console, refresh_per_second=1) as live:
       if generated != old:
         old = str(generated)
         try:
@@ -128,21 +155,21 @@ async def main():
             async with TelegramClient(StringSession(telegram.auth.string), telegram.app.id, telegram.app.hash, receive_updates=False) as client:
               await client(UpdateProfileRequest(about=generated))
         except Exception as e:
-            live.update("[bold red]\[MAIN] Failed to edit bio to [bold blue]{}[/bold blue]! ([bold blue]{}[/bold blue]: [bold blue]{}[/bold blue])[/bold red]".format(generated, type(e).__name__, str(e)))
+            live.update(logstring("didntedit").format(generated, type(e).__name__, str(e)))
             live.stop()
-        live.update("[MAIN] Changed bio to [bold blue]{}[/bold blue]".format(generated))
+        live.update(logstring("edited").format(generated))
       else:
-        live.update("[MAIN] Generated bio is identical to cached bio ([bold blue]{}[/bold blue]), not changing bio".format(generated))
+        live.update(logstring("cached").format(generated))
       live.stop()
-    with console.status("Sleeping...", spinner="point"):
+    with console.status(logstring("sleeping"), spinner="point"):
       await sleep(script.delay)
 
 
-
+keep_alive()
 try:
   result = run(check_auth())
   if result[0] is False:
-    console.log("[MAIN] Failed to authenticate."+(" ({}: {})".format(type(result[1]).__name__, (str(result[1]))) if result[1] is not None else ""))
+    console.log(logstring("didntauth")+(" ({}: {})".format(type(result[1]).__name__, (str(result[1]))) if result[1] is not None else ""))
   else:
     add_plugins()
     run(main())
